6876a0f06b7ece9372b5430497dcfda3820ee219
Abhishek <abhishekchaurasia.avi@gmail.com>
2022-12-29 14:02:17 +0530

    Java Programs Answer update

--- Assignments/Java Programs Answer/src/com/Abhishek/Armstrong.java
+++ Assignments/Java Programs Answer/src/com/Abhishek/Armstrong.java
@@ -1,5 +1,24 @@
 //To find Armstrong Number between two given number.
 package com.Abhishek;
-
+import java.util.Scanner;
 public class Armstrong {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = in.nextInt();
+        int original = num;
+        int sum = 0;
+
+        while(num > 0){
+            int rem = num % 10;
+            num = num / 10;
+            sum = sum + rem*rem*rem;
+
+        }
+        if(sum == original){
+            System.out.println("The number is Armstrong number");
+        }else{
+            System.out.println("The number is not an Armstrong number: ");
+        }
+    }
 }
--- Assignments/Java Programs Answer/src/com/Abhishek/Palindrome.java
+++ Assignments/Java Programs Answer/src/com/Abhishek/Palindrome.java
@@ -1,5 +1,19 @@
 //To find out whether the given String is Palindrome or not.
 package com.Abhishek;
-
+import java.util.*;
 public class Palindrome {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        String str = in.next();
+        StringBuffer buffer = new StringBuffer(str);
+        buffer.reverse();
+        String data = buffer.toString();
+        if(str.equals(data)){
+            System.out.println("Given String is palindrome");
+        } else {
+            System.out.println("Given String is not palindrome");
+        }
+    }
+
 }
