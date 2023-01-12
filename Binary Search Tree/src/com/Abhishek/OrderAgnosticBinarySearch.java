2de75df7a9ef30cd6650ff6c3910f9e5cceb8e06
Abhishek <abhishekchaurasia.avi@gmail.com>
2023-01-12 18:32:11 +0530

    changes in Bs

--- Binary Search Tree/src/com/Abhishek/OrderAgnosticBinarySearch.java
+++ Binary Search Tree/src/com/Abhishek/OrderAgnosticBinarySearch.java
@@ -2,40 +2,43 @@ package com.Abhishek;
 
 public class OrderAgnosticBinarySearch {
     public static void main(String[] args) {
-        int[] arr = {2, 3, 4, 18, 35, 60, 19, 81, 65, 89, 99, 5, 18, 20, 30, 50, 80};
-        int target = 89;
-        System.out.println("The element found at index: "+orderAgnosticBS(arr, target) );
+//        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int[] arr = {99, 80, 75, 22, 11, 10, 5, 2, -3};
+        int target = 22;
+        int ans = orderAgnosticBS(arr, target);
+        System.out.println(ans);
     }
-    static  int orderAgnosticBS(int[] arr, int target){
+
+    static int orderAgnosticBS(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
-//  find weather the order is ascending or descending
+
+        // find whether the array is sorted in ascending or descending
         boolean isAsc = arr[start] < arr[end];
 
-        while(start <= end){
-//            find the middle element
-//            int mid = ((start + end)/2);might be possible (start + end) exceed the range of in in java
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
             int mid = start + (end - start) / 2;
 
-            if(arr[mid] == target){
+            if (arr[mid] == target) {
                 return mid;
             }
 
-            if(isAsc){
-                if(target < arr[mid]){
+            if (isAsc) {
+                if (target < arr[mid]) {
                     end = mid - 1;
-                }else {
+                } else {
                     start = mid + 1;
                 }
-            }else
-            if(target > arr[mid]){
-                end = mid - 1;
-            }else {
-                start = mid + 1;
+            } else {
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
             }
-
         }
         return -1;
-
     }
 }
