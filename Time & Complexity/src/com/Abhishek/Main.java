12dad96c71e69456ae425dd6c069bbe5c5b6be8f
Abhishek <abhishekchaurasia.avi@gmail.com>
2023-02-07 16:49:44 +0530

    Time Complexity

--- Time & Complexity/src/com/Abhishek/Main.java
+++ Time & Complexity/src/com/Abhishek/Main.java
@@ -3,6 +3,12 @@ package com.Abhishek;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(fiboFormula(50));
     }
+//    Program of Fibonacci Series using Golden Ratio
+    static int fiboFormula(int n){
+        // just for demo, use long instead
+        return (int) ((Math.pow(((1+Math.sqrt(5))/2),n)-Math.pow(((1-Math.sqrt(5))/2),n))/Math.sqrt(5));
+    }
+
 }
