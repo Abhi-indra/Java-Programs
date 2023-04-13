b1f5be442b3ccba0ad108db42f967045049de0e5
Abhishek <abhishekchaurasia.avi@gmail.com>
2023-04-13 14:19:52 +0530

    2D

--- Arrays/src/TwoDArray.java
+++ Arrays/src/TwoDArray.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class TwoDArray {
+import java.util.*;
+public class TwoDArray {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the row");
+        int row = in.nextInt();
+        System.out.println("Enter the coloumn");
+        int col = in.nextInt();
+
+        int[][] numbers = new int[row][col];
+//        input
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                numbers[i][j] = in.nextInt();
+            }
+        }
+//        output
+        for (int i = 0; i < row ; i++) {
+            for (int j = 0; j < col ; j++) {
+                System.out.print(numbers[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
